64feee0d279533b86b728087cc6a48dd764d5bbe
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2025-02-18 11:38:20 +0700

    perf: :art: improve middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,20 @@
-export { auth as middleware } from "@/lib/auth";
+import { auth } from "@/lib/auth";
+
+export default auth((req) => {
+  const auth = req.auth;
+  const currentPath = req.nextUrl.pathname;
+
+  if (currentPath === "/signin" && !!auth) {
+    const newUrl = new URL("/", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
