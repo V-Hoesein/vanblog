88a1602044cc7def7dab91c288023d83982377cb
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2025-03-19 15:32:56 +0700

    feat: :sparkles: Add navigation constants and update dependencies in package.json

---
+++ constans.ts
@@ -0,0 +1,16 @@
+import { NavProps } from "./types/globals";
+
+export const NAV_MENU: NavProps[] = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "create",
+    path: "/create/blog",
+  },
+  {
+    name: "my writing",
+    path: "/my writing",
+  },
+];
