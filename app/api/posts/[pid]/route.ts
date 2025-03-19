5fbe9a63c0dc4aea0fb7800e2d22773cd714788a
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2025-03-19 14:09:44 +0700

    feat: :sparkles: Add PUT route for updating a post by ID with validation and error handling

--- app/api/posts/[pid]/route.ts
+++ app/api/posts/[pid]/route.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@/lib/client";
-import { NextResponse } from "next/server";
+import postSchema from "@/lib/zod/post";
+import { NextRequest, NextResponse } from "next/server";
 
 const GET = async (_: Request, { params }: { params: { pid?: string } }) => {
   try {
@@ -26,6 +27,58 @@ const GET = async (_: Request, { params }: { params: { pid?: string } }) => {
     });
   } catch (error) {
     console.error("Error fetching post", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+};
+
+const PUT = async (
+  request: NextRequest,
+  { params }: { params: { pid?: string } }
+) => {
+  try {
+    const { pid } = params;
+    if (!pid) {
+      return NextResponse.json(
+        { message: "Post ID is required" },
+        { status: 400 }
+      );
+    }
+
+    const post = await prisma.post.findUnique({ where: { id: pid } });
+    if (!post) {
+      return NextResponse.json(
+        { message: "Post not found", data: null },
+        { status: 404 }
+      );
+    }
+
+    const body = await request.json();
+    const validate = postSchema.partial().safeParse(body);
+
+    if (!validate.success) {
+      return NextResponse.json(
+        {
+          message: "Validation error",
+          errors: validate.error.flatten().fieldErrors,
+        },
+        { status: 400 }
+      );
+    }
+
+    const updatedPost = await prisma.post.update({
+      where: { id: pid },
+      data: validate.data,
+    });
+
+    return NextResponse.json(
+      { message: "Post updated successfully", data: updatedPost },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error updating post:", error);
     return NextResponse.json(
       { message: "Internal Server Error" },
       { status: 500 }
@@ -64,4 +117,4 @@ const DELETE = async (_: Request, { params }: { params: { pid?: string } }) => {
   }
 };
 
-export { GET, DELETE };
+export { GET, PUT, DELETE };
