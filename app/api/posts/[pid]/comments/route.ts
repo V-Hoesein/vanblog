0da58b860158ca438a4d0a2591e300ab13b2eb9f
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2025-03-19 15:32:28 +0700

    feat: :sparkles: Add GET route for fetching comments by post ID with error handling

---
+++ app/api/posts/[pid]/comments/route.ts
@@ -0,0 +1,34 @@
+import { prisma } from "@/lib/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (
+  _: NextRequest,
+  { params }: { params: { pid?: string } }
+) => {
+  try {
+    const { pid } = params;
+
+    if (!pid) {
+      return NextResponse.json(
+        { message: "Missing postId parameter" },
+        { status: 400 }
+      );
+    }
+
+    const postComments = await prisma.comment.findMany({
+      where: { postId: pid },
+      include: { comentator: true },
+    });
+
+    return NextResponse.json({
+      message: "Comments fetched successfully",
+      data: postComments,
+    });
+  } catch (error) {
+    console.error("Error fetching comments:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error", data: null },
+      { status: 500 }
+    );
+  }
+};
