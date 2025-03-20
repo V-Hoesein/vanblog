70f616a621fa4df829b6f614e5dfd958a52b9b66
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2025-03-20 12:18:21 +0700

    feat: :sparkles: Add POST route to like and unlike posts with validation and error handling

--- app/api/posts/[pid]/likes/route.ts
+++ app/api/posts/[pid]/likes/route.ts
@@ -34,3 +34,70 @@ export const GET = async (
     );
   }
 };
+
+export const POST = async (
+  request: NextRequest,
+  { params }: { params: Promise<{ pid: string }> }
+) => {
+  try {
+    const { pid } = await params;
+
+    if (!pid) {
+      return NextResponse.json(
+        { message: "Missing postId parameter" },
+        { status: 400 }
+      );
+    }
+
+    const body = await request.json();
+    const { userId } = body;
+
+    if (!userId) {
+      return NextResponse.json(
+        { message: "Missing userId parameter" },
+        { status: 400 }
+      );
+    }
+
+    const existPost = await prisma.post.findUnique({
+      where: { id: pid },
+    });
+
+    if (!existPost) {
+      return NextResponse.json({ message: "Post not found" }, { status: 404 });
+    }
+
+    const existingLike = await prisma.like.findFirst({
+      where: { postId: pid, userId },
+    });
+
+    if (existingLike) {
+      const unlike = await prisma.like.delete({
+        where: { id: existingLike.id },
+      });
+
+      return NextResponse.json(
+        { message: "Post unliked successfully", data: unlike },
+        { status: 201 }
+      );
+    }
+
+    const liked = await prisma.like.create({
+      data: {
+        postId: pid,
+        userId,
+      },
+    });
+
+    return NextResponse.json(
+      { message: "Post liked successfully", data: liked.postId },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log("Error liking post:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+};
