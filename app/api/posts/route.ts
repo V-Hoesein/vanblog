a5685edd57fb2d46466ebc667cc89e1b2f75f35c
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2025-04-03 13:24:02 +0700

    feat: :sparkles: Enhance POST route for posts with improved validation, author existence check, and structured error handling

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/client";
 import { NextRequest, NextResponse } from "next/server";
 import postSchema from "@/lib/zod/post";
+import { validationHandler } from "@/lib/zod/validationHandler";
 
 export const GET = async () => {
   try {
@@ -28,27 +29,49 @@ export const GET = async () => {
 export const POST = async (request: NextRequest) => {
   try {
     const body = await request.json();
-    const validate = postSchema.safeParse(body);
+    const validate = validationHandler(postSchema, body);
+
     if (!validate.success) {
+      throw new Error("Validation failed: Data is undefined");
+    }
+    const author = await prisma.user.findUnique({
+      where: { id: validate.data.authorId },
+    });
+
+    if (!author) {
       return NextResponse.json(
         {
-          message: "Validation error",
-          errors: validate.error.flatten().fieldErrors,
+          message: "Author not found",
+          status: "error",
+          data: null,
         },
-        { status: 400 }
+        { status: 404 }
       );
     }
 
-    const post = await prisma.post.create({ data: validate.data });
+    const post = await prisma.post.create({
+      data: {
+        ...validate.data,
+        authorId: validate.data.authorId,
+      },
+    });
 
     return NextResponse.json(
-      { message: "Post created successfully", data: post.title },
+      {
+        message: "Post created successfully",
+        status: "success",
+        data: post.title,
+      },
       { status: 201 }
     );
   } catch (error) {
     console.error("Error creating post:", error);
     return NextResponse.json(
-      { message: "Internal Server Error" },
+      {
+        message: "Internal Server Error",
+        status: "error",
+        data: null,
+      },
       { status: 500 }
     );
   }
